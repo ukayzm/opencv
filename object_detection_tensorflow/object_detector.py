b60e0fc2311bddbeafd4c4905c52f64e79c01a28
Kijong Uhm <kijong.uhm@gmail.com>
2018-05-22 12:24:15 +0900

    working with Mask RCNN

--- object_detection_tensorflow/object_detector.py
+++ object_detection_tensorflow/object_detector.py
@@ -10,6 +10,7 @@ import six.moves.urllib as urllib
 
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+from object_detection.utils import ops as utils_ops
 
 
 class ObjectDetector():
@@ -64,9 +65,6 @@ class ObjectDetector():
         categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=self.NUM_CLASSES, use_display_name=True)
         self.category_index = label_map_util.create_category_index(categories)
 
-        self.detection_graph.as_default()
-        self.sess = tf.Session(graph=self.detection_graph)
-
         self.frame_count = 0
 
     def __del__(self):
@@ -86,7 +84,7 @@ class ObjectDetector():
           # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(frame, axis=0)
           # Actual detection.
-        (boxes, scores, classes, num) = self.sess.run(
+        (boxes, scores, classes, num) = sess.run(
               [detection_boxes, detection_scores, detection_classes, num_detections],
               feed_dict={image_tensor: image_np_expanded})
           # Visualization of the results of a detection.
@@ -119,6 +117,79 @@ class ObjectDetector():
 
         return frame
 
+    def detect_objects2(self, image_np):
+        with self.detection_graph.as_default():
+            with tf.Session(graph=self.detection_graph) as sess:
+                self.detect_objects_(image_np, sess, self.detection_graph)
+
+    def detect_objects_(self, image_np, sess, detection_graph):
+        ops = tf.get_default_graph().get_operations()
+        all_tensor_names = {output.name for op in ops for output in op.outputs}
+        tensor_dict = {}
+        for key in [
+              'num_detections', 'detection_boxes', 'detection_scores',
+              'detection_classes', 'detection_masks'
+          ]:
+            tensor_name = key + ':0'
+            if tensor_name in all_tensor_names:
+                tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+        if 'detection_masks' in tensor_dict:
+            # The following processing is only for single image
+            detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+            detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+            # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+            real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+            detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+            detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+            detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                detection_masks, detection_boxes, image_np.shape[0], image_np.shape[1])
+            detection_masks_reframed = tf.cast(
+                tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+            # Follow the convention by adding back the batch dimension
+            tensor_dict['detection_masks'] = tf.expand_dims(
+                detection_masks_reframed, 0)
+        image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+        output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image_np, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+        output_dict['num_detections'] = int(output_dict['num_detections'][0])
+        output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+        if 'detection_masks' in output_dict:
+            output_dict['detection_masks'] = output_dict['detection_masks'][0]
+
+        vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          output_dict['detection_boxes'],
+          output_dict['detection_classes'],
+          output_dict['detection_scores'],
+          self.category_index,
+          instance_masks=output_dict.get('detection_masks'),
+          use_normalized_coordinates=True,
+          line_thickness=1)
+
+
+    def get_frame2(self):
+        # Grab a single frame of video
+        frame = self.camera.get_frame()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = frame
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        self.detect_objects2(rgb_small_frame)
+
+        return frame
+
     def get_jpg_bytes(self):
         frame = self.get_frame()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
@@ -129,10 +200,10 @@ class ObjectDetector():
 
 
 if __name__ == '__main__':
-    detector = ObjectDetector('ssd_mobilenet_v1_coco_2017_11_17')
-    #detector = ObjectDetector('mask_rcnn_inception_v2_coco_2018_01_28')
+    #detector = ObjectDetector('ssd_mobilenet_v1_coco_2017_11_17')
+    detector = ObjectDetector('mask_rcnn_inception_v2_coco_2018_01_28')
     while True:
-        frame = detector.get_frame()
+        frame = detector.get_frame2()
 
         # show the frame
         cv2.imshow("Frame", frame)
