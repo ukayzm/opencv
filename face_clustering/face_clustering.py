082a28116fe3f457fdbf0188affc0275bffbd828
Kijong Uhm <kijong.uhm@gmail.com>
2018-07-24 21:08:29 +0900

    face-clustering 2

--- face_clustering/face_clustering.py
+++ face_clustering/face_clustering.py
@@ -2,37 +2,72 @@
 import face_recognition
 import cv2
 
-class FaceClustering():
-    def __init__(self):
-        self.frame_no = 0
-        self.results = []
+class Face():
+    def __init__(self, frame_no, name, box, encoding):
+        self.frame_no = frame_no
+        self.name = name
+        self.box = box
+        self.encoding = encoding
 
-    def quantify(self, rgb):
-        self.frame_no += 1
+class FaceClustering():
+    def quantify(self, frameId, rgb):
+        faces_in_frame = []
         boxes = face_recognition.face_locations(rgb, model="hog")
         encodings = face_recognition.face_encodings(rgb, boxes)
         for box, encoding in zip(boxes, encodings):
-            result = [self.frame_no, None, box, encoding]
-            self.results.append(result)
-            print(result)
+            face = Face(frameId, None, box, encoding)
+            faces_in_frame.append(face)
+        return faces_in_frame
+
+    def drawBox(self, frame, faces_in_frame):
+        # Draw a box around the face
+        for face in faces_in_frame:
+            (top, right, bottom, left) = face.box
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
 
 if __name__ == '__main__':
-    import time
+    import math
+    import datetime
 
     cam = cv2.VideoCapture(0)
+    frameRate = cam.get(5)
+    print("frameRate =", frameRate)
+    frame_no = 0
+    prevFrameId = frame_no // math.floor(frameRate)
     fc = FaceClustering()
 
-    while True:
+    faces = []
+
+    while (cam.isOpened()):
         ret, frame = cam.read()
+        frame_no += 1
+        frameId = frame_no // math.floor(frameRate)
+        if (frameId == prevFrameId):
+            continue
+        prevFrameId = frameId
+        print("frameId =", frameId)
+
         rgb = frame[:, :, ::-1]
-        fc.quantify(rgb)
+        faces_in_frame = fc.quantify(frameId, rgb)
+
+        #if (len(faces_in_frame) == 0):
+        #    continue
+
+        print(faces_in_frame)
+
+        # show the frame
+        fc.drawBox(frame, faces_in_frame)
+        cv2.imshow("Frame", frame)
+
+        faces.extend(faces_in_frame)
 
         # if the `q` key was pressed, break from the loop
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
 
-        time.sleep(1)
-
+    # do a bit of cleanup
+    cam.release()
+    cv2.destroyAllWindows()
     print('finish')
